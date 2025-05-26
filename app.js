8f17a2a9f69701e25b3cabd1f2af9b708ba90928
xOAviOx <avishuklacode@gmail.com>
2025-05-26 13:26:31 +0530

    Finalised

--- app.js
+++ app.js
@@ -6,20 +6,61 @@ const viewRouter = require("./routes/viewRoutes");
 const userRouter = require("./routes/userRoutes");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
+const helmet = require("helmet");
+// const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const compression = require("compression");
+
 const path = require("path");
 const app = express();
+const rateLimit = require("express-rate-limit");
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-// Middleware
-app.use(express.urlencoded({ extended: true }));
+// middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(methodOverride("_method")); // Add method-override middleware
+app.use(methodOverride("_method"));
 
 //serving static files
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'"],
+      scriptSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      workerSrc: ["'self'", "blob:"],
+      objectSrc: [],
+      imgSrc: ["'self'", "blob:", "data:", "https:"],
+      fontSrc: ["'self'"],
+    },
+  })
+);
+
+//limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+// //data sanitization against nosql query injection
+// app.use(mongoSanitize());
+
+//data sanitization against XSS
+// app.use(xss());
+
+//prevent parameter pollution
+app.use(hpp());
+
+app.use(compression());
 
 app.use("/api/v1/users/", userRouter);
 app.use("/api/v1/blogs/", blogRouter);
